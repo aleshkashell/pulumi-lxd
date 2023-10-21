0f10d46170baf69443df5f5752bfcfe7548cd170
Sheludchenkov Aleksei <aleksei.sheludchenkov@altium.com>
2023-10-21 16:53:55 +0300

    Refresh sdk

--- sdk/dotnet/Volume.cs
+++ sdk/dotnet/Volume.cs
@@ -65,6 +65,7 @@ namespace Pulumi.Lxd
             var defaultOptions = new CustomResourceOptions
             {
                 Version = Utilities.Version,
+                PluginDownloadURL = "https://github.com/aleshkashell/pulumi-lxd/releases/",
             };
             var merged = CustomResourceOptions.Merge(defaultOptions, options);
             // Override the ID if one was specified for consistency with other language SDKs.
