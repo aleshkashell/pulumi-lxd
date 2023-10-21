1697e63c2fc010764eeb5996ad6b6a8e0dace51a
Sheludchenkov Aleksei <aleksei.sheludchenkov@altium.com>
2023-10-21 23:30:36 +0300

    Refresh sdk

--- sdk/go/lxd/config/config.go
+++ sdk/go/lxd/config/config.go
@@ -4,7 +4,7 @@
 package config
 
 import (
-	"github.com/pulumi/pulumi-lxd/sdk/go/lxd/internal"
+	"github.com/aleshkashell/pulumi-lxd/sdk/go/lxd/internal"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
